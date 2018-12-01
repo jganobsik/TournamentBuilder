201361a7a2491c747f125c0c3898d785722c565b
jganobsik <39414138+jganobsik@users.noreply.github.com>
2018-11-30 22:36:28 -0500

    Add files via upload

---
+++ TournamentBuilder.js
@@ -0,0 +1,33 @@
+/* this is a basic tournament builder based off a VBA project
+I did for a VBA class i took in college. I will be using this
+project to learn versioning and become accustomed to gradually building
+programs over time. 
+
+The tournament builder takes a list of players and available teams, and randomly
+assigns a team to each player. I used this program to create a Madden tournament,
+where participants were randomly assigned a NFL team to use in a playoff-style
+tournament bracket. */
+
+// Version .01 - basic variables and function names created
+
+
+// initialize empty arrays to house both players and teams
+var players = [];
+var teams = [];
+var entries = [];
+
+function addPlayer(name) {
+// prompts the tournament organizer for the name of a player, and adds the input to the players array
+
+}
+
+function addTeam(name) {
+//prompts the tournament organizer for the name of a team, and adds the input to the team array
+
+}
+
+function assignTeams(players, teams) {
+//assigns a team to each player, and returns an object with the player and team to the entries array.
+//removes the player and team from their respective arrays
+
+}
