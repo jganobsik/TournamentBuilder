ccfc9008994506eabc6c89eaf6b60d523aa7f220
jganobsik <39414138+jganobsik@users.noreply.github.com>
2018-12-02 22:18:00 -0500

    Add files via upload

--- TournamentBuilder.js
+++ TournamentBuilder.js
@@ -9,6 +9,7 @@ where participants were randomly assigned a NFL team to use in a playoff-style
 tournament bracket. */
 
 // Version .01 - basic variables and function names created
+// Version .05 - added basic structure of functions
 
 
 // initialize empty arrays to house both players and teams
@@ -18,16 +19,26 @@ var entries = [];
 
 function addPlayer(name) {
 // prompts the tournament organizer for the name of a player, and adds the input to the players array
-
+players.push(name);
 }
 
 function addTeam(name) {
 //prompts the tournament organizer for the name of a team, and adds the input to the team array
-
+teams.push(name);
 }
 
 function assignTeams(players, teams) {
 //assigns a team to each player, and returns an object with the player and team to the entries array.
-//removes the player and team from their respective arrays
+//removes the team from the array
+for(let i = 0; i < players.length; i++){
+    let randomLottery = Math.floor(Math.random() * teams.length - 1)
+    entries.push({
+        "player": players[i],
+        "team": teams[randomLottery],
+        
+    });
+    teams.splice(randomLottery, 1);
+}
+
 
 }
